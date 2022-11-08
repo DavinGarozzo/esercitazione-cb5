70d66497b18d4c8cc8d36f81e060cb3770fe4dfb
DavinGarozzo <salvatoredavingarozzo@gmail.com>
2022-11-08 18:12:59 +0100

    esercitazione-08-11-22

---
+++ 08-11-22/script.js
@@ -0,0 +1,80 @@
+const bodyEl = document.querySelector("body");
+
+const createCard = (data) => {
+  const cardEl = document.createElement("div");
+  const cardImgEl = document.createElement("img");
+  const cardId = document.createElement("h3");
+  const cardNameEl = document.createElement("h1");
+  const typeEl = document.createElement("h3");
+
+  cardEl.className = "cardpoke";
+  cardImgEl.setAttribute("src", data.sprites.other.dream_world.front_default);
+  cardImgEl.setAttribute("alt", "img");
+  cardNameEl.textContent = data.name;
+  cardId.textContent = data.id;
+  typeEl.textContent = data.types[0].type.name;
+
+  switch (data.types[0].type.name) {
+    case "electric":
+      cardEl.style = "background-color: #FCF7DE";
+      break;
+    case "water":
+      cardEl.style = "background-color: #DEF3FD";
+      break;
+    case "ground":
+      cardEl.style = "background-color: #f4e7da";
+      break;
+    case "rock":
+      cardEl.style = "background-color: #d5d5d4";
+      break;
+    case "fairy":
+      cardEl.style = "background-color: #fceaff";
+      break;
+    case "poison":
+      cardEl.style = "background-color: #98d7a5";
+      break;
+    case "bug":
+      cardEl.style = "background-color: #f8d5a3";
+      break;
+    case "dragon":
+      cardEl.style = "background-color: #97b3e";
+      break;
+    case "psychic":
+      cardEl.style = "background-color: #eaeda1";
+      break;
+    case "flying":
+      cardEl.style = "background-color: #F5F5F5";
+      break;
+    case "fighting":
+      cardEl.style = "background-color: #E6E0D4";
+      break;
+    case "normal":
+      cardEl.style = "background-color: #F5F5F5";
+      break;
+    case "fire":
+      cardEl.style = "background-color: #FDDFDF";
+      break;
+    case "grass":
+      cardEl.style = "background-color: #DEFDE0";
+      break;
+    case "ghost":
+      cardEl.style = "background-color: #705898";
+      break;
+    case "ice":
+      cardEl.style = "background-color: #98d8d8";
+      break;
+  }
+  //appendElements
+  cardEl.append(cardImgEl, cardId, cardNameEl, typeEl);
+  bodyEl.append(cardEl);
+};
+
+fetch("https://pokeapi.co/api/v2/pokemon/?limit=150")
+  .then((res) => res.json())
+  .then((poke) => {
+    poke.results.forEach((item) =>
+      fetch(item.url).then((res) =>
+        res.json().then((pokemon) => createCard(pokemon))
+      )
+    );
+  });
