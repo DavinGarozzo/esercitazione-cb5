abf8130c4fc1e05d7af115a464dc7a6deb0086c9
DavinGarozzo <salvatoredavingarozzo@gmail.com>
2023-01-09 18:06:04 +0100

    esercitazione 09-01-23

---
+++ 09-01-23/todo-list/src/components/todo-items/TodoItem.jsx
@@ -0,0 +1,14 @@
+export function TodoItem(props) {
+  const { children, isDone, changeStatus, index } = props;
+
+  const clickHandler = () => {
+    changeStatus(index, isDone);
+  };
+
+  return (
+    <li>
+      {isDone ? "✅" : "❌"} -<span>{children}</span>
+      <button onClick={clickHandler}>{isDone ? "Un-Done" : "Done"}</button>
+    </li>
+  );
+}
